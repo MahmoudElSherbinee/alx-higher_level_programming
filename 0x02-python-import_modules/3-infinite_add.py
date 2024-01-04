de3f923d5e15875f159cce63d857414d55fd3d0f
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-01-04 21:24:25 +0200

    second 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     arguments = sys.argv
     res = 0
-    for i in range (len(arguments) - 1):
+    for i in range(len(arguments) - 1):
         res = res + int(arguments[i+1])
     print("{:d}".format(res))
