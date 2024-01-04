10fc9f42e777402e8ed935f85ff5a51d8139e827
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-01-04 21:23:53 +0200

    first 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    res = 0
+    for i in range (len(arguments) - 1):
+        res = res + int(arguments[i+1])
+    print("{:d}".format(res))
