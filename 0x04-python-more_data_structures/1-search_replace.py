720afdd6f1ae2ab5ef924eb8cc96a5daff103c30
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-01-10 20:26:42 +0200

    first 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#new_list = list(map(lambda x: replace if x == search else x, my_list))
-#return new_list
+# new_list = list(map(lambda x: replace if x == search else x, my_list))
+# return new_list
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(my_list)):
