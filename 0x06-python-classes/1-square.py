6aeee1cd79986bbc41ffc42d09fe2f085d4c35c6
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-01-23 18:15:04 +0200

    first 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Creating a class called Square """
+
+
+class Square:
+    """ Define a private instance attr size """
+
+    def __init__(self, size):
+        """ creating the size attr """
+        self.__size = size
