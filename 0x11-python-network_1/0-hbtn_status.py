a485e43f31c61badc20a65c5884f8946f168b254
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-04-28 18:25:09 +0300

    auto push

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Python script that fetches https://alx-intranet.hbtn.io/status
-# 
+#
 # You must use the package urllib
 # You are not allowed to import any packages other than urllib
 # The body of the response must be displayed
