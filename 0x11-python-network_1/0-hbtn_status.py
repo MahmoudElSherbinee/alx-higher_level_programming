1b612e918c8e9fb122e03e21c7d9838599349d55
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-04-28 18:28:59 +0300

    auto push

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,17 +3,20 @@
 #
 # You must use the package urllib
 # You are not allowed to import any packages other than urllib
-# The body of the response must be displayed
+# The the_result of the response must be displayed
 # like the following example (tabulation before -)
 # You must use a with statement
 
-import urllib.request
+from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    my_request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(my_request) as response:
+    url = "https://alx-intranet.hbtn.io/status"
+    my_request = Request(url)
+
+    with urlopen(my_request) as response:
         the_result = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(the_result)))
-        print("\t- content: {}".format(the_result))
-        print("\t- utf8 content: {}".format(the_result.decode("UTF-8")))
+
+    print("Body response:")
+    print("\t- type: {}".format(type(the_result)))
+    print("\t- content: {}".format(the_result))
+    print("\t- utf8 content: {}".format(the_result.decode("utf-8")))
